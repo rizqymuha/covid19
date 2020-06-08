a56d424af6076711ec999936b193b294357ee6c0
ryzkymd <ryzkysam@gmail.com>
2020-06-08 10:57:30 +0700

    destructuring object json

--- script/main.js
+++ script/main.js
@@ -1,39 +1,37 @@
 const baseURL = "https://covid19.mathdro.id";
 
 function formatNumber(num) {
-    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    if (num != null) {
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    }
 }
 
 const getData = fetch(`${baseURL}/api`)
     .then(response => response.json())
     .then(response => {
-        const dataPositive = response.confirmed.value;
-        const dataRecovered = response.recovered.value;
-        const dataDeaths = response.deaths.value
+        const { confirmed, recovered, deaths } = response
 
-        const positive = document.querySelector('.positive');
-        const recovered = document.querySelector('.recovered');
-        const deaths = document.querySelector('.deaths');
+        const conf = document.querySelector('.positive');
+        const reco = document.querySelector('.recovered');
+        const deat = document.querySelector('.deaths');
 
-        positive.innerHTML = formatNumber(dataPositive);
-        recovered.innerHTML = formatNumber(dataRecovered);
-        deaths.innerHTML = formatNumber(dataDeaths);
+        conf.innerHTML = formatNumber(confirmed.value);
+        reco.innerHTML = formatNumber(recovered.value);
+        deat.innerHTML = formatNumber(deaths.value);
     });
 
 const getDataIndo = fetch(`${baseURL}/api/countries/indonesia`)
     .then(response => response.json())
     .then(response => {
-        const dataIndoPositive = response.confirmed.value;
-        const dataIndoRecovered = response.recovered.value;
-        const dataIndoDeaths = response.deaths.value;
+        const { confirmed, recovered, deaths } = response
 
-        let indoPositive = document.querySelector('.indo-positive');
-        let indoRecovered = document.querySelector('.indo-recovered');
-        let indoDeaths = document.querySelector('.indo-deaths');
+        const indoConfirmed = document.querySelector('.indo-positive');
+        const indoRecovered = document.querySelector('.indo-recovered');
+        const indoDeaths = document.querySelector('.indo-deaths');
 
-        indoPositive.innerHTML = formatNumber(dataIndoPositive);
-        indoRecovered.innerHTML = formatNumber(dataIndoRecovered);
-        indoDeaths.innerHTML = formatNumber(dataIndoDeaths);
+        indoConfirmed.innerHTML = formatNumber(confirmed.value);
+        indoRecovered.innerHTML = formatNumber(recovered.value);
+        indoDeaths.innerHTML = formatNumber(deaths.value);
     });
 
 const getDataAllCountry = fetch('https://api.kawalcorona.com')
@@ -42,11 +40,12 @@ const getDataAllCountry = fetch('https://api.kawalcorona.com')
         trDataGlobal = '';
         for (let i = 0; i <= response.length; i++) {
             let no = i + 1;
-            trDataGlobal += showDataGlobal(response[i].attributes, no)
+            if (response[i] != null) {
+                trDataGlobal += showDataGlobal(response[i].attributes, no)
+            }
             const tbDataGlobal = document.querySelector('.tb-data-global');
             tbDataGlobal.innerHTML = trDataGlobal;
         }
-        console.log(response[1].attributes.Country_Region);
     });
 
 function showDataGlobal(results, no) {
@@ -62,14 +61,8 @@ function showDataGlobal(results, no) {
     `
 }
 
-const getDataProvIndo = fetch(`${baseURL}/api/confirmed`)
-    .then(response => response.json())
-    .then(response => {
-        console.log(response);
-    });
-
 const today = new Date();
-const monthName = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November'];
+const monthName = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
 let newDate = `${today.getDate()} ${monthName[today.getMonth()]} ${today.getFullYear()}`
 let newTime = `${today.getHours()}:${today.getMinutes()}:${today.getMilliseconds()}`
 const lastUpdate = document.querySelector('.last-update');
